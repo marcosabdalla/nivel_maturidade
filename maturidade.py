f3eb79e529af3aba4306703c4b39a5cbf0af35b6
marcosabdalla <48099375+marcosabdalla@users.noreply.github.com>
2026-08-17 21:16:04 -0300

    Update maturidade.py

--- maturidade.py
+++ maturidade.py
@@ -116,7 +116,7 @@ with gov:
         mt = st.text_input('Meta')
         Pess = st.selectbox('Responsável', options = nomes)
         idx = pessoas[pessoas["nome"] == Pess].index
-        idx_pess = pessoas.loc[idx,"id"]
+        idx_pess = pessoas.loc[idx,"id"].values[0]
         stat = ["Não iniciado","Em andamento","Concluido"]
         Stat = st.selectbox('Status', options = stat)
     obs = FORM.text_area("Observações", value="")
