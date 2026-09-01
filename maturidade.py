8be4af9b7bf962659432fc08058b9c1688682ea1
marcosabdalla <48099375+marcosabdalla@users.noreply.github.com>
2026-09-01 00:11:34 -0300

    maturidade.py

--- maturidade.py
+++ maturidade.py
@@ -199,7 +199,7 @@ with gov:
      
 #--------------------------------------- Nova Entrada ----------------------------------------------
     df_A = le_ferramentas_eco()
-    df_B = le_ferramentas_lob()
+    df_B = le_ferramentas_log()
     new_df = pd.concat([df_A["Ferramentas"], df_B["Ferramenta"]], axis=1)
     st.dataframe(new_df)
     FORM = st.form('Novo Reg. Gov', clear_on_submit = True)
